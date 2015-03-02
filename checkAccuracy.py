771ad24d22631de5956a6d7d7de9143f0b3813a2
Dan <dannysilver3@gmail.com>
2015-03-01 20:16:40 -0600

    Get the output files in order, cant sort on the string unfortunately

--- checkAccuracy.py
+++ checkAccuracy.py
@@ -1,4 +1,5 @@
 from cli import *
+import os
 
 def main():
   cli = Interface()
@@ -12,3 +13,19 @@ def getTotalMassAndMomentum(particles):
     mass += particle.mass
     momentum += mass * particle.velocityMagnitude()
   return (momentum, mass)
+
+# Given a prefix like "output", it finds all files in the current directory with the prefix
+def getOutputFiles(prefix):
+  return [filename for filename in os.listdir('.') if filename.startswith(prefix)]
+
+
+# Takes an array of filenames ['output-100.csv', 'output-001.csv']
+# Returns the numbers [100, 1]
+def getOutputFileNumbers(filenames, prefix):
+  return [int(f.replace(prefix+'-', '').replace('.csv', '')) for f in filenames]
+
+
+# Takes an array of filenumbers, sorts them, and adds in the prefix
+def filenamesInOrder(filenumbers, prefix):
+  filenumbers.sort()
+  return [prefix + "-" + str(n) + '.csv' for n in filenumbers]
