ab9cc857a640a817cc6bab8f262c16d368f0f042
Dan <dannysilver3@gmail.com>
2015-03-01 20:25:18 -0600

    All functions now called from main.  Fiven a prefix, it prints all the filenames in order

--- checkAccuracy.py
+++ checkAccuracy.py
@@ -1,12 +1,23 @@
 from cli import *
 import os
 
-def main():
-  cli = Interface()
-  cli.args.ifile = 'example.csv'
-  ppos = cli.readInputFile()
-  print getTotalMassAndMomentum(ppos)
+def main(prefix = 'output'):
+  allOutputFiles = getOutputFiles(prefix)
+  timesteps      = getOutputFileNumbers(allOutputFiles, prefix)
+  filenames      = fileNamesInOrder(timesteps, prefix)
+  return filenames
 
+
+
+# Given a filename (output-001.csv), return the total mass and momentum
+def totalsFromFile(filename):
+  cli = Interface()
+  cli.args.ifile = filename
+  ppos = cli.readInputFile()
+  return getTotalMassAndMomentum(ppos)
+
+
+# Given an array of particles, return the total mass and momentum
 def getTotalMassAndMomentum(particles):
   momentum = mass = 0
   for particle in particles:
@@ -18,14 +29,15 @@ def getTotalMassAndMomentum(particles):
 def getOutputFiles(prefix):
   return [filename for filename in os.listdir('.') if filename.startswith(prefix)]
 
-
 # Takes an array of filenames ['output-100.csv', 'output-001.csv']
 # Returns the numbers [100, 1]
 def getOutputFileNumbers(filenames, prefix):
   return [int(f.replace(prefix+'-', '').replace('.csv', '')) for f in filenames]
 
+# Takes an array of file numbers, sorts them, and adds in the prefix
+def fileNamesInOrder(file_numbers, prefix):
+  file_numbers.sort()
+  return [prefix + "-" + str(n) + '.csv' for n in file_numbers]
 
-# Takes an array of filenumbers, sorts them, and adds in the prefix
-def filenamesInOrder(filenumbers, prefix):
-  filenumbers.sort()
-  return [prefix + "-" + str(n) + '.csv' for n in filenumbers]
+
+print main()
