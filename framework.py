d9cb6960d78cc1841a81cd1da9ee78e5b50fdf07
tguard <tpgardner19@gmail.com>
2015-03-09 23:32:44 -0500

    Added support for saving particles within main framework.py loop

--- framework.py
+++ framework.py
@@ -1,6 +1,5 @@
 from particle import Particle
 import numpy as np
-import cli
 
 """
 This is the framework for iterating over a list of particles, computing particle accelerations, and numerically integrating their equations of motion.
@@ -25,14 +24,19 @@ def Newtonian_gravity(p,q):
 	R = np.sqrt(r.dot(r)) # magnitude of the separation vector
 	return ((CONST_G * q.mass) / (R**3)) * r
 
+def saveParticles(particles, fname):
+        fhandle = open(fname, "w")
+        for p in particles:
+                p.writeToFile(fhandle)
+        fhandle.close()
 
-def sim(particles, bound, kernel, maxiter, pnum, smooth, t_norm, x_norm, interval):
-    CONST_H = 1.0   # size of timestep (this should come from config file)
-#    CONST_T_MAX = 10    # max iteration time (this should come from config file)
-    CONST_T_MAX = maxiter
-    t = 0.0     # elapsed time
+def sim(particles, bound, kernel, maxiter, pnum, smooth, t_norm, x_norm, interval, savefile):
+        CONST_H = 1.0   # size of timestep (this should come from config file)
+        #    CONST_T_MAX = 10    # max iteration time (this should come from config file)
+        CONST_T_MAX = maxiter
+        t = 0.0     # elapsed time
 
-    '''
+        '''
 	So we can do this one of two ways.
 	1) Keep only one copy of the system in memory.
 	   This is what is implemented here. This does not require
@@ -42,37 +46,43 @@ def sim(particles, bound, kernel, maxiter, pnum, smooth, t_norm, x_norm, interva
 	   This way we can remove a for-loop, but requires more memory.
 	Luckily, it is likely these problems only affect the serial algorithm.
 	'''
-	
-    save = 0
-    while(t < CONST_T_MAX):
-        # while CONST_T_MAX = 60 and interval = 10
-        if (save*interval) == t:
-            print "[+] Saved @ iteration %d" % int(t)
-            save = save+1
+        # output-100.csv = prefix + interval + file extension
+	ary = savefile.split(".")  # only split savefile once ([0]=prefix, [1]=extension)
+        save = 0
+        print "[+] Saved @ iterations: ",
+        while(t < CONST_T_MAX):
+                if (save*interval) == t:
+                        fname = "%s-%d.%s" % (ary[0], int(t), ary[1])
+                        save += 1  # bump save counter
+                        string = "\b%d..." % int(t)     # '\b' prints a backspace character to remove previous space
+                        print string,
+                        saveParticles(particles, fname)
+                                
+                # main simulation loop
+                for p in particles:
+                        # preemptively start the Velocity Verlet computation (first half of velocity update part)
+                        p.vel += (CONST_H/2.0) * p.acc
+                        temp = p.acc
+                        p.acc = 0
+                        for q in particles:
+                                # Calculate gravitational pull for each particle on every OTHER particle
+                                if(p.id != q.id):
+                                        p.acc += Newtonian_gravity(p,q)
+                                
+                        # finish velocity update
+                        p.vel += (CONST_H/2.0) * p.acc
+                        '''
+                        Velocity Verlet integration: Works only assuming force is velocity-independent
+                        http://en.wikipedia.org/wiki/Verlet_integration#Velocity_Verlet
+                        '''
+                        for p in particles:
+                        	# perform position update
+                        	p.pos += CONST_H * (p.vel + (CONST_H/2.0)*temp)
+                                
+                t += CONST_H  # advance time
 
-		# main simulation loop
-        for p in particles:
-			# preemptively start the Velocity Verlet computation (first half of velocity update part)
-			p.vel += (CONST_H/2.0) * p.acc
-			temp = p.acc
-			p.acc = 0
-			for q in particles:
-			    # Calculate gravitational pull for each particle on every OTHER particle
-				if(p.id != q.id):
-					p.acc += Newtonian_gravity(p,q)
-			# finish velocity update
-			p.vel += (CONST_H/2.0) * p.acc
-
-        '''
-        Velocity Verlet integration: Works only assuming force is velocity-independent
-        http://en.wikipedia.org/wiki/Verlet_integration#Velocity_Verlet
-        '''
-
-        for p in particles:
-        	# perform position update
-        	p.pos += CONST_H * (p.vel + (CONST_H/2.0)*temp)
-
-        t += CONST_H # advance time
-
-    print "[+] Saved @ iteration %d \t(last iteration)\n" % int(t)
-    return t    # returns the last t-value, which is useful for displaying total iterations
+        # Always save the last interval
+        print "\b%d\n" % int(t)
+        fname = "%s-%d.%s" % (ary[0], int(t), ary[1])
+        saveParticles(particles, fname)
+        return t    # returns the last t-value, which is useful for displaying total iterations
