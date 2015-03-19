f01d304d458be0cd740fd5bcb9ad068eec5fb1dd
Dan <dannysilver3@gmail.com>
2015-03-18 20:42:11 -0500

    No idea how this file got here, different project

--- svm_test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from sklearn.svm import SVC
-X = np.array([[-1, -1,5], [-2, -1,5], [1, 1,4], [2, 1,3]])
-y = np.array([1, 1, 2, 4])
-clf = SVC(C=1.0, cache_size=2000)
-clf.fit(X, y) 
-print(clf.predict([[-0.8, -1,4]]))
