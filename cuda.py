83866711e7ec723a9e332f6babc0f92f38324206
Dan Silver <dannysilver3@gmail.com>
2015-04-02 01:26:17 +0000

    Code cleanup part 3

--- cuda.py
+++ cuda.py
@@ -21,6 +21,8 @@ class DoubleOpStruct:
 datasets = {
   "id": None,
   "mass": None,
+  "rho": None,
+  "pressure": None,
   "pos_x": None,
   "pos_y": None,
   "pos_z": None,
@@ -34,7 +36,11 @@ datasets = {
 
 # Iterate through datasets and allocate memory on the device
 for dataset_name in datasets:
-    datasets[dataset_name] = cuda.mem_alloc(DoubleOpStruct.mem_size)
+    datasets[dataset_name] = {
+        "gpu_ptr": cuda.mem_alloc(DoubleOpStruct.mem_size),
+        "input": [],
+        "results": None
+    }
 
 # Create particles
 particles = []
@@ -42,60 +48,41 @@ for x in range(0, 4):
     p = Particle(x,x*2,x*3,x*4,x,x,x,x)
     particles.append(p)
 
-# unpack the particles
-ids = []
-mass = []
-rho = []
-pressure = []
-
-pos_x = []
-pos_y = []
-pos_z = []
-
-vel_x = []
-vel_y = []
-vel_z = []
-
-acc_x = []
-acc_y = []
-acc_z = []
-
 # populate the arrays with the particle data
 for particle in particles:
-    ids.append(particle.id)
-    mass.append(particle.mass)
-    rho.append(particle.rho)
-    pressure.append(particle.pressure)
+    datasets['id']['input'].append(particle.id)
+    datasets['mass']['input'].append(particle.mass)
+    datasets['rho']['input'].append(particle.rho)
+    datasets['pressure']['input'].append(particle.pressure)
 
-    pos_x.append(particle.pos[0])
-    pos_y.append(particle.pos[1])
-    pos_z.append(particle.pos[2])
+    datasets['pos_x']['input'].append(particle.pos[0])
+    datasets['pos_y']['input'].append(particle.pos[1])
+    datasets['pos_z']['input'].append(particle.pos[2])
 
-    vel_x.append(particle.vel[0])
-    vel_y.append(particle.vel[1])
-    vel_z.append(particle.vel[2])
+    datasets['vel_x']['input'].append(particle.vel[0])
+    datasets['vel_y']['input'].append(particle.vel[1])
+    datasets['vel_z']['input'].append(particle.vel[2])
 
-    acc_x.append(particle.acc[0])
-    acc_y.append(particle.acc[1])
-    acc_z.append(particle.acc[2])
+    datasets['accel_x']['input'].append(particle.acc[0])
+    datasets['accel_y']['input'].append(particle.acc[1])
+    datasets['accel_z']['input'].append(particle.acc[2])
 
-id_array = DoubleOpStruct(numpy.array(ids, dtype=numpy.int16), datasets['id'])
-mass_array = DoubleOpStruct(numpy.array(mass, dtype=numpy.float64), datasets['mass'])
-pos_x_array = DoubleOpStruct(numpy.array(pos_x, dtype=numpy.float64), datasets['pos_x'])
-pos_y_array = DoubleOpStruct(numpy.array(pos_y, dtype=numpy.float64), datasets['pos_y'])
-pos_z_array = DoubleOpStruct(numpy.array(pos_z, dtype=numpy.float64), datasets['pos_z'])
-vel_x_array = DoubleOpStruct(numpy.array(vel_x, dtype=numpy.float64), datasets['vel_x'])
-vel_y_array = DoubleOpStruct(numpy.array(vel_y, dtype=numpy.float64), datasets['vel_y'])
-vel_z_array = DoubleOpStruct(numpy.array(vel_z, dtype=numpy.float64), datasets['vel_z'])
-accel_x_array = DoubleOpStruct(numpy.array(acc_x, dtype=numpy.float64), datasets['accel_x'])
-accel_y_array = DoubleOpStruct(numpy.array(acc_y, dtype=numpy.float64), datasets['accel_y'])
-accel_z_array = DoubleOpStruct(numpy.array(acc_z, dtype=numpy.float64), datasets['accel_z'])
-
+datasets['id']['result'] = DoubleOpStruct(numpy.array(datasets['id']['input'], dtype=numpy.int16), datasets['id']['gpu_ptr'])
+datasets['mass']['result'] = DoubleOpStruct(numpy.array(datasets['mass']['input'], dtype=numpy.float64), datasets['mass']['gpu_ptr'])
+datasets['pos_x']['result'] = DoubleOpStruct(numpy.array(datasets['pos_x']['input'], dtype=numpy.float64), datasets['pos_x']['gpu_ptr'])
+datasets['pos_y']['result'] = DoubleOpStruct(numpy.array(datasets['pos_y']['input'], dtype=numpy.float64), datasets['pos_y']['gpu_ptr'])
+datasets['pos_z']['result'] = DoubleOpStruct(numpy.array(datasets['pos_z']['input'], dtype=numpy.float64), datasets['pos_z']['gpu_ptr'])
+datasets['vel_x']['result'] = DoubleOpStruct(numpy.array(datasets['vel_x']['input'], dtype=numpy.float64), datasets['vel_x']['gpu_ptr'])
+datasets['vel_y']['result'] = DoubleOpStruct(numpy.array(datasets['vel_y']['input'], dtype=numpy.float64), datasets['vel_y']['gpu_ptr'])
+datasets['vel_z']['result'] = DoubleOpStruct(numpy.array(datasets['vel_z']['input'], dtype=numpy.float64), datasets['vel_z']['gpu_ptr'])
+datasets['accel_x']['result'] = DoubleOpStruct(numpy.array(datasets['accel_x']['input'], dtype=numpy.float64), datasets['accel_x']['gpu_ptr'])
+datasets['accel_y']['result'] = DoubleOpStruct(numpy.array(datasets['accel_y']['input'], dtype=numpy.float64), datasets['accel_y']['gpu_ptr'])
+datasets['accel_z']['result'] = DoubleOpStruct(numpy.array(datasets['accel_z']['input'], dtype=numpy.float64), datasets['accel_z']['gpu_ptr'])
 
 print "original arrays"
-print id_array
-print mass_array
-print pos_x_array
+print datasets['id']['result']
+print datasets['mass']['result']
+print datasets['pos_x']['result']
 mod = SourceModule("""
     struct data_array {
         int array_length;
@@ -118,9 +105,9 @@ mod = SourceModule("""
     }
     """)
 func = mod.get_function("double_array")
-func(datasets['id'], datasets['mass'], datasets['pos_x'], block = (len(particles), 1, 1), grid=(1, 1))
+func(datasets['id']['gpu_ptr'], datasets['mass']['gpu_ptr'], datasets['pos_x']['gpu_ptr'], block = (len(particles), 1, 1), grid=(1, 1))
 
 print "doubled arrays"
-print id_array
-print mass_array
-print pos_x_array
+print datasets['id']['result']
+print datasets['mass']['result']
+print datasets['pos_x']['result']
