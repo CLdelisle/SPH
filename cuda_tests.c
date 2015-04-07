aa47087a647612e5125f324127eb5fe666f68e0c
Dan Silver <dannysilver3@gmail.com>
2015-04-07 03:24:25 +0000

    test #2 for multiple particles, tests thread access index

--- cuda_tests.c
+++ cuda_tests.c
@@ -16,3 +16,20 @@ __global__ void increment_particle_properties(ParticleArray *particle_array) {
     p->rho      += 1;
     p->pressure += 1;
 }
+
+__global__ void increment_particle_properties_on_multiple_particles(ParticleArray *particle_array) {
+    Particle* p = particle_array->ptr + threadIdx.x;
+    p->id       += 1;
+    p->mass     += 1;
+    p->pos[0]   += 1;
+    p->pos[1]   += 1;
+    p->pos[2]   += 1;
+    p->vel[0]   += 1;
+    p->vel[1]   += 1;
+    p->vel[2]   += 1;
+    p->acc[0]   += 1;
+    p->acc[1]   += 1;
+    p->acc[2]   += 1;
+    p->rho      += 1;
+    p->pressure += 1;
+}
